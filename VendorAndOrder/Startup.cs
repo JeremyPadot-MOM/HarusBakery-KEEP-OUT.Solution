e8fa4628eeb2e54516d976037d336c8f8d728a10
Jeremy Kale Padot <jeremy@padot.us>
2020-03-13 15:35:40 -0700

    update startup.cs to use static files

--- VendorAndOrder/Startup.cs
+++ VendorAndOrder/Startup.cs
@@ -33,7 +33,7 @@ namespace VendorAndOrder
           name: "default",
           template: "{controller=Home}/{action=Index}/{id?}");
       });
-
+        app.UseStaticFiles();
         app.Run(async (context) =>
         {
           await context.Response.WriteAsync("Something went wrong!");
