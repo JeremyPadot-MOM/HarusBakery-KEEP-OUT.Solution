92dfe8f9b88bcc2929706d29cba38756439b0f28
Jeremy Kale Padot <jeremy@padot.us>
2020-03-13 09:43:00 -0700

    add order list with ids

--- VendorAndOrder/Models/Order.cs
+++ VendorAndOrder/Models/Order.cs
@@ -4,6 +4,36 @@ namespace VendorAndOrder.Models
 {
   public class Order
   {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
 
+    public Order( string title, string description, int price, string date)
+    {
+        Title = title;
+        Description = description;
+        Price = price;
+        Date = date;
+        _instances.Add(this);
+        Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+        return _instances;
+    }
+
+    public static void ClearAll()
+    {
+        _instances.Clear();
+    }
+
+    public static Order Find(int searchId)
+    {
+        return _instances[searchId - 1];
+    }
   }
 }
