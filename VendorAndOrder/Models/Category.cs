0864c2f56acbc9be423db09483a003c818d21555
Jeremy Kale Padot <jeremy@padot.us>
2020-03-13 09:52:11 -0700

    add category(vendor) lists/ will change category to vendor

--- VendorAndOrder/Models/Category.cs
+++ VendorAndOrder/Models/Category.cs
@@ -4,6 +4,39 @@ namespace VendorAndOrder.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public Vendor(string vendorName, string vendorDescription)
+    {
+      Name = vendorName;
+      Description = vendorDescription;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
     
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
+    }
   }
 }
