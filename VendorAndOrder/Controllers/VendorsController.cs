6b4f85d22cea3627ddabe921060b12be4410cca7
Jeremy Kale Padot <jeremy@padot.us>
2020-03-13 11:47:08 -0700

    add vendorsController routes

--- VendorAndOrder/Controllers/VendorsController.cs
+++ VendorAndOrder/Controllers/VendorsController.cs
@@ -8,5 +8,49 @@ namespace VendorAndOrder.Controllers
   public class VendorsController : Controller
   {
     
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+    List<Vendor> allVendors = Vendor.GetAll();
+    return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendors.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+
+    // This one creates new Items within a given Category, not new Categories:
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, int orderPrice, string orderDate)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderTitle, orderDescription, orderPrice, orderDate);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
